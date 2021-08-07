18c23148dd6436dc6c75a4cda7b20e64183af381
javorske <jaywors@gmail.com>
2021-08-07 11:01:04 +0200

    Created Movement script
    Created Movement script that will allow the player to move the Rocket.

---
+++ Rocket Escape/Assets/Scripts/Movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("pressed space");
+        }
+    }
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("preseed left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("preseed right");
+        }
+    }
+}
