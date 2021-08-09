7edc4a38c62150f95f86bac0ebe05eb54593761c
javorske <jaywors@gmail.com>
2021-08-09 13:30:37 +0200

    Update CollisionHandler.cs
    Added code that makes the scene reload after colliding with anything other than objects with the tag Friendly or Finish.

--- Rocket Escape/Assets/Scripts/CollisionHandler.cs
+++ Rocket Escape/Assets/Scripts/CollisionHandler.cs
@@ -1,6 +1,21 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
-
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("It's just a freindly object");
+                break;
+            case "Finish":
+                Debug.Log("Touched the landing pad");
+                break;
+            default:
+                SceneManager.LoadScene(0);
+                break;
+        }
+    }
 }
