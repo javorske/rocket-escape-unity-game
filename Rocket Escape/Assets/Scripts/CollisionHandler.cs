47378cc6c2475f2c70465e32da45bbe958e37b4a
javorske <jaywors@gmail.com>
2021-08-09 15:23:34 +0200

    Update CollisionHandler.cs
    Created StartFinishSequence, StartCrashSequence and LoadNextLevel.

--- Rocket Escape/Assets/Scripts/CollisionHandler.cs
+++ Rocket Escape/Assets/Scripts/CollisionHandler.cs
@@ -1,8 +1,10 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] float delay = 1f;
     void OnCollisionEnter(Collision collision)
     {
         switch (collision.gameObject.tag)
@@ -11,13 +13,36 @@ public class CollisionHandler : MonoBehaviour
                 Debug.Log("It's just a freindly object");
                 break;
             case "Finish":
-                Debug.Log("Touched the landing pad");
+                StartFinishSequence();
                 break;
             default:
-                ReloadLevel();
+                StartCrashSequence();
                 break;
         }
     }
+    void StartFinishSequence()
+    {
+        GetComponent<Movement>().enabled = false;
+        Invoke("LoadNextLevel", delay);
+    }
+    void StartCrashSequence()
+    {
+        GetComponent<Movement>().enabled = false;
+        Invoke("ReloadLevel", delay);
+    }
+
+    void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
     void ReloadLevel()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
