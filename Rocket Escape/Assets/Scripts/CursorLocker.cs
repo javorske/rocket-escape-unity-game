e33a3bc4d589abc0f4695fe1a03224ab316c32a2
javorske <jaywors@gmail.com>
2021-08-14 13:38:49 +0200

    Created CursorLocker.cs script
    Created CursorLocker.cs script that locks cursor and makes it invisible. Attached it to Rocket prefab.

---
+++ Rocket Escape/Assets/Scripts/CursorLocker.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorLocker : MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+}
